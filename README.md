f9d1f226de9cd6087bc9e37eeccd863d2d872157
J. Kniffki <26249992+kifirifis@users.noreply.github.com>
2020-10-27 15:46:56 +0100

    fix script's plto and readme

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Track Covid-19 cases in Krakow
 
 Main data stored in CVS file under data/krakow.csv and is updated daily except on weekends. 
 
-Please mention us like #KrakowCovid and/or names in Twitter if you find this useful :)
+Please mention us like #KrakowCovid and/or names in Twitter if you find this useful :blush:
 
 Mainly maintained by: 
-J. Kniffki @kifirifis
+J. Kniffki @JKniffki
 J. Hernandez @peelbox
 
 Any contributions are welcomed
--- scripts/#plots.R
+++ scripts/#plots.R
@@ -1,33 +1,40 @@
                         #plots
-library(tidyverse)
-library(ggrepel) #Previene que se encimen las etiquetas
-library(gganimate) #animados
+if (!("dplyr" %in% installed.packages())) {
+        install.packages("dplyr")
+}
+if (!("lubridate" %in% installed.packages())) {
+        install.packages("lubridate")
+}
+if (!("ggplot2" %in% installed.packages())) {
+        install.packages("ggplot2")
+}
+if (!("ggrepel" %in% installed.packages())) {
+        install.packages("ggrepl")
+}
 
-datos <- read.csv("poland/data/definitivo.csv") 
+datos <- read.csv("data/krakow.csv") 
 datos$fecha <- lubridate::as_date(datos$fecha)
+`%>%` <- dplyr::`%>%` #definición del pipe 
+fecha <- Sys.Date()
 
-#============= Plot ======
-unique(datos$distrito)
-datos %>% filter(distrito == "M.kraków") %>% 
+#============= Active cases today ================
+datos %>% dplyr::filter(distrito == "M.kraków") %>% #Filtro
         
-        #plot
-ggplot(aes(fecha, activos)) +
-        geom_line() +
-        geom_point() +
-        geom_text_repel(aes(label = as.character(activos))) + 
+        #============= Gráfico ========= 
+ggplot2::ggplot(ggplot2::aes(fecha, activos)) +
+        ggplot2::geom_line() +
+        ggplot2::geom_point() +
+        ggrepel::geom_text_repel(ggplot2::aes(label = as.character(activos))) + 
         
         #escalas
-        scale_x_date(date_breaks = "2 day", date_labels = "%b %d") + 
+        ggplot2::scale_x_date(date_breaks = "2 day", date_labels = "%b %d") + 
         
         # Etiquetas
-        labs(title = "Evolución de casos activos en M. Kraków",
-             subtitle = "Actualización: 23-10-2020",
-             caption = "Elaboración: @JKniffki | KStats®",
-             x = "", y = "") -> p1
-p1
-ggsave("poland/plots/casosact.png")
+        ggplot2::labs(title = "Active Cases in M. Kraków",
+             subtitle = paste("Update:", fecha, "| Data source: https://wsse.krakow.pl"),
+             caption = "Author: @JKniffki | KStats®",
+             x = "", y = "Active Cases") -> p1
 
+#Guardar en /plots
+ggplot2::ggsave(paste0("plots/active", fecha, ".png"), p1)
 
-#========== plot animado 
-# p1 + transition_reveal(fecha)
-# anim_save("poland/plots/animact.gif")
